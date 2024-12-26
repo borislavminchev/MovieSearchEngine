ec955e1e5065a7bacc13a61b70be85202ac558e9
borislavminchev <59176170+borislavminchev@users.noreply.github.com>
2024-12-26 15:51:44 +0200

    add query for sorted by occurrence

--- indexing/index_data.py
+++ indexing/index_data.py
@@ -11,22 +11,22 @@ es = Elasticsearch(
 df = pd.read_csv("./preprocessed_movies_clean.csv")
 
 
-# actions = [
-#     {
-#         "_index": "movies",
-#         "_id": doc["id"],
-#         "_source": doc
-#     } 
-#     for doc in df.to_dict(orient="records")
-# ]
+actions = [
+    {
+        "_index": "movies",
+        "_id": doc["id"],
+        "_source": doc
+    } 
+    for doc in df.to_dict(orient="records")
+]
 
-# try:
-#     helpers.bulk(es, actions)
-# except helpers.BulkIndexError as e:
-#     print(f"Bulk indexing error: {e}")
-#     print("Failed documents:")
-#     for error in e.errors:
-#         print(error)
+try:
+    helpers.bulk(es, actions)
+except helpers.BulkIndexError as e:
+    print(f"Bulk indexing error: {e}")
+    print("Failed documents:")
+    for error in e.errors:
+        print(error)
 
 #
 # query = {
@@ -55,20 +55,42 @@ df = pd.read_csv("./preprocessed_movies_clean.csv")
 #   }
 # }
 
-query = {
-    "query": {
-        "match": {
-            "search_content": "star"
-        }
-    }
-}
+# query = {
+#     "query": {
+#         "match": {
+#             "search_content": "star"
+#         }
+#     }
+# }
 
+# query_sorted = {
+#   "_source": ["search_content"],
+#     "query": {
+#         "match": {
+#             "search_content": "star"
+#         }
+#     },
+#   "sort": [
+#     {
+#       "_script": {
+#         "type": "number",
+#         "script": {
+#           "source": "String target = params.target; int count = 0; if (doc['search_content.raw'].size() > 0) { String text = doc['search_content.raw'].value; int index = 0; while ((index = text.indexOf(target, index)) != -1) { count++; index += target.length(); } } return count;",
+#           "params": {
+#             "target": "star"
+#           }
+#         },
+#         "order": "desc"
+#       }
+#     }
+#   ]
+# }
 
-try:
-    response = es.search(index="movies", body=query)
-    print(response["hits"]['total']['value'])
-    for hit in response["hits"]["hits"]:
-        print(hit["_source"]["title"], hit["_score"],  " ----> ", hit["_source"]["search_content"].lower().count("star wars"))
-except Exception as e:
-    print(f"Error: {e}")
+# try:
+#     response = es.search(index="movies", body=query_sorted)
+#     print(response["hits"]['total']['value'])
+#     for hit in response["hits"]["hits"]:
+#         print(hit["_id"], hit["sort"][0],  " ----> ", hit["_source"]["search_content"].lower().count("star"))
+# except Exception as e:
+#     print(f"Error: {e}")
 
